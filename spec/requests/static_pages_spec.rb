3f417f989218f31f4730ba99e05de82e77748209
William Haw <z59sgs@gmail.com>
2014-09-11 14:38:43 +0000

    Added user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -50,4 +50,20 @@ describe "Static pages" do
     expect(page).to have_title(full_title(''))
   end
 
+  describe "for signed-in users" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+      FactoryGirl.create(:micropost, user:user, content: "Dolor dit amet")
+      sign_in user
+      visit root_path
+    end
+
+    it "should render the user's feed" do
+      user.feed.each do |item|
+        expect(page).to have_selector("li##{item.id}", text: item.content)
+      end
+    end
+  end
+
 end
